460f332df5b2fb65496e9d8d522aa2719cc2a66d
AlpalcaLad <olliespamed@gmail.com>
2026-06-12 13:22:19 +0100

    pygame setup work

--- game.py
+++ game.py
@@ -5,6 +5,24 @@ import qr_read
 import keyboard
 import os
 from cards import *
+import pygame
+import sys
+import random
+
+class sprite():
+    def __init__(self,parent,game,asset):
+        self.p = parent
+        self.g = game
+        self.img = pygame.image.load(asset).convert_alpha()
+
+    def draw(self):
+        self.g.screen.blit(
+            self.img,
+            (
+                self.parent.x,
+                self.parent.y
+            ),
+        )
 
 class entity():
     def __init__(self):
@@ -12,6 +30,8 @@ class entity():
         self.block = 0
         self.effects = []
         self.name = "entity"
+        self.x = 0
+        self.y = 0
 
     def damage(self,dmg):
         #naive formula
@@ -45,9 +65,23 @@ class player(entity):
     def play(cardText):
         pass
 
+colours = {
+    "black": (0,0,0),
+    "white": (255,255,255)
+}
+
 class game():
     def __init__(self):
-        pass
+        #general setup
+        self.run = True
+        #basic pygame setup
+        pygame.init()
+        self.W, self.H = 600, 600
+        self.screen = pygame.display.set_mode((self.W,self.H))
+        pygame.display.set_caption("Slay the Spire IRL")
+        self.clock = pygame.time.Clock()
+        font = pygame.font.SysFont(None, 36)
+        #UI definition
 
     def readCard(self,cardText: str):
         if cardText.startswith("player"):
@@ -56,3 +90,17 @@ class game():
             pass #play card for given player
         return
     
+    def mainloop(self,conn):
+        while self.run:
+            self.screen.fill(colours["black"])
+
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    pygame.quit()
+                    self.run=False
+            
+            #render UI
+
+            #render players
+
+            #render events
